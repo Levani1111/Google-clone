e2aa0894ddc2e6ea85fcbabd0c7fe04ad2ade838
Levani Papashvili <papashvililevani@gmail.com>
2020-10-08 19:03:57 -0400

    added folders pages , components and in pages added Home.js-Home.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     // BEN
     <div className="app">
       <h1>Google-clone</h1>
+      <Home />
     </div>
   );
 }
