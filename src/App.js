7b8ae830040ce5f9054522cb4fb8e0bc0cfb80f3
Levani Papashvili <papashvililevani@gmail.com>
2020-10-08 19:31:05 -0400

    add Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     // BEN
     <div className="app">
-      <h1>Google-clone</h1>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1> thi is the search page </h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
