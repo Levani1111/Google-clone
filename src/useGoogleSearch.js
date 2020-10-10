30f9a69f8fc8e5329414f4025ea8f6bd4c0fbb54
Levani Papashvili <papashvililevani@gmail.com>
2020-10-10 15:48:32 -0400

    Craeted custom hook

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -10,8 +10,13 @@ const useGoogleSearch = (term) => {
     const fetchData = async () => {
       fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}$q=${term}`
-      );
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+        });
     };
+    fetchData();
   }, [term]);
 };
 
