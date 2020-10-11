7a27c5f286454f6639564c74632ead1f07f60771
Levani Papashvili <papashvililevani@gmail.com>
2020-10-11 09:28:05 -0400

    add icons in searchPage

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -6,6 +6,12 @@ import useGoogleSearch from "../useGoogleSearch";
 import Response from "../response";
 import { Link } from "react-router-dom";
 import Search from "../Components/Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function SearchPage() {
   // eslint-disable-next-line no-unused-vars
@@ -29,6 +35,35 @@ function SearchPage() {
         <div className="searchPage__headerBody">
           <Search hideButtons />
         </div>
+        <div className="searchPage__options"></div>
+        <div className="searchPage__optionsLeft">
+          <div className="searchPage__option">
+            <SearchIcon />
+            <Link to="/all">All</Link>
+          </div>
+          <div className="searchPage__option">
+            <DescriptionIcon />
+            <Link to="/news">News</Link>
+          </div>
+          <div className="searchPage__option">
+            <ImageIcon />
+            <Link to="/images">Images</Link>
+          </div>
+          <div className="searchPage__option">
+            <LocalOfferIcon />
+            <Link to="/shopping">shopping</Link>
+          </div>
+          <div className="searchPage__option">
+            <RoomIcon />
+            <Link to="/maps">Maps</Link>
+          </div>
+          <div className="searchPage__option">
+            <MoreVertIcon />
+            <Link to="/more">More</Link>
+          </div>
+        </div>
+
+        <div className="searchPage__optionsRight"></div>
       </div>
       <div className="searchPage__results">{/* results */}</div>
     </div>
