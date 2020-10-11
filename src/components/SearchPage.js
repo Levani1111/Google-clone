07221f6314e3eefff5a50690015bfb2a009944b4
Levani Papashvili <papashvililevani@gmail.com>
2020-10-11 17:05:36 -0400

    grtting images form data

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -85,7 +85,18 @@ function SearchPage() {
 
           {data?.items.map((item) => (
             <div className="searchPage__result">
-              <a href={item.link}>{item.displayLink}</a>
+              <a href={item.link}>
+                {item.pagemap?.cse_image?.length > 0 &&
+                  item.pagemap?.cse_image[0]?.src && (
+                    <img
+                      className="searchPage__resultImage"
+                      src={item.pagemap?.cse_image[0]?.src}
+                      alt=""
+                    />
+                  )}
+
+                {item.displayLink}
+              </a>
               <a className="searchPage__resultTitle" href={item.link}>
                 <h2>{item.title}</h2>
               </a>
